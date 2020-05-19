82fda588787960e67eb753894a13ee26d48ad95d
July Angelica Moreno Jara <july.moreno@globant.com>
2020-05-19 15:15:50 -0500

    [Update] - New addition method for Calculator class

--- src/com/academy/git/Calculator.java
+++ src/com/academy/git/Calculator.java
@@ -4,14 +4,26 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        System.out.println("Si no están estos cambios hay tabla!");
+        System.out.println("Si no estan estos cambios hay tabla!");
 
         int number1 = 7;
         int number2 = 4;
+        int number3 = 2;
+        int number4 = 1;
+        int number5 = 3;
 
         int sum = sum(number1, number2);
         System.out.println("The sum of " + number1 + " and " +
                 number2 + " equals: " + sum);
+       
+        sum = sum(number1, number2, number3);
+        System.out.println("The sum of " + number1 + " and " + number2 + " and " + number3 + " equals: " + sum);
+        
+        sum = sum(number1, number2, number3, number4);
+        System.out.println("The sum of " + number1 + " and " + number2 + " and " + number3 + " and " + number4 + " equals: " + sum);
+        
+        sum = sum(number1, number2, number3, number4, number5);
+        System.out.println("The sum of " + number1 + " and " + number2 + " and " + number3 + " and " + number4 + " and " + number5 + " equals: " + sum);
     }
 
     public static int sum(int num1, int num2) {
@@ -29,4 +41,12 @@ public class Calculator {
         return num1 * num2;
     }
 	
+	public static int sum(int... numbers) {
+		int total=0;
+		for (int i : numbers) {
+			total = sum(total, i);
+		}
+		return total;
+	
+	}
 }
